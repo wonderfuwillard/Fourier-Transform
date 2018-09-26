aa4c8e96993247879fdbab18348985c83c768862
Willard <wonderfulwillard@gmail.com>
2018-09-26 10:31:13 +0800

    Update image_compression.m

--- image_compression.m
+++ image_compression.m
@@ -10,7 +10,7 @@ T = 20;
 %% Display the reconstructed compressed images
 figure(1);
 
-D = 20;  % compression ratio of 3:1
+D = 20;  % compression ratio of 1:1
 G = myJPEGCompress(f, T, D);
 fc = myJPEGDecompress(G, T, D);
 imshow(fc, [0 255]);
@@ -31,7 +31,7 @@ title(['Compression Ratio = ' num2str(3) ':1']);
 
 figure(3);
 
-D = 9;  % compression ratio of 3:1
+D = 9;  % compression ratio of 5:1
 G = myJPEGCompress(f, T, D);
 fc = myJPEGDecompress(G, T, D);
 imshow(fc, [0 255]);
@@ -39,14 +39,14 @@ title(['Compression Ratio = ' num2str(5) ':1']);
 
 figure(4);
 
-D = 6;  % compression ratio of 3:1
+D = 6;  % compression ratio of 10:1
 G = myJPEGCompress(f, T, D);
 fc = myJPEGDecompress(G, T, D);
 imshow(fc, [0 255]);
 title(['Compression Ratio = ' num2str(10) ':1']);
 figure(5);
 
-D = 4;  % compression ratio of 3:1
+D = 4;  % compression ratio of 25:1
 G = myJPEGCompress(f, T, D);
 fc = myJPEGDecompress(G, T, D);
 imshow(fc, [0 255]);
